c7f0c18c2d56fbe340441bb5d5c018a31133cffc
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-20 19:10:33 -0400

    create prompts for the questions to ask the user how they want their password, prompts don't do anything yet, they are just there

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,28 @@
 // Assignment Code
+var passLength
+
+var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l','m','n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var specialChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', '/', ';', ':', '"', '<', '>', ',', '.', '?' ];
+
+var numberChars =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+var choices = [upperCase, lowerCase, specialChars, numberChars]
+
+var passLength = window.prompt('how many characters would you like your password to be?');
+
+var useNumbers = window.confirm('Do you want to include numbers? (Ok = yes)');
+
+var useUpper = window.confirm('Do you want to include UPPER case letters?(Ok = yes)');
+
+var useLower = window.confirm('Do you want to include LOWER case Leters?(Ok = yes)')
+
+var useSpecial = window.confirm('Do you want to inlcude SPECIAL characters?(Ok = yes)');
+    
+
+
 var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
@@ -18,3 +42,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
