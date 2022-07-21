2c5c5ec6710eed46c508eabeefe21c5d3c95b0a5
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-21 18:58:19 -0400

    im able to generate the randomized letters, but not the desired password length yet

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var passLength
+var generateBtn = document.querySelector('#generate');
 
 var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
@@ -9,29 +9,40 @@ var specialChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', 
 
 var numberChars =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-var choices = [upperCase, lowerCase, specialChars, numberChars]
-
-var passLength = window.prompt('how many characters would you like your password to be?');
-
-var useNumbers = window.confirm('Do you want to include numbers? (Ok = yes)');
-
-var useUpper = window.confirm('Do you want to include UPPER case letters?(Ok = yes)');
-
-var useLower = window.confirm('Do you want to include LOWER case Leters?(Ok = yes)')
-
-var useSpecial = window.confirm('Do you want to inlcude SPECIAL characters?(Ok = yes)');
+var choices = [upperCase, lowerCase, specialChars, numberChars];
     
 
+//prompts for the user
+var passwordLength = 0;
+    window.prompt('How many characters would you like your password to be?(8-128)');
+    if ((passwordLength < 8) && (passwordLength > 128)){
+    window.alert('not a valid input');
+    }
 
-var generateBtn = document.querySelector('#generate');
-
+var useUpper = window.confirm('Do you want to include UPPER case letters?(Ok = yes)');
+   
+var useLower = window.confirm('Do you want to include LOWER case Leters?(Ok = yes)');
+    
+var useNumbers = window.confirm('Do you want to include numbers? (Ok = yes)');
+    
+var useSpecial = window.confirm('Do you want to inlcude SPECIAL characters?(Ok = yes)');
+   
 function generatePassword() {
-  var password = 'password';
+  var password = "";
   // TODO: add code to generate the password here
+  for ( var i = 0; i <= passwordLength; i++){
+    password = password + upperCase[Math.floor(Math.random() * upperCase.length)];
+    password = password + lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password = password + numberChars[Math.floor(Math.random() * numberChars.length)];
+    password = password + specialChars[Math.floor(Math.random() * specialChars.length)];
+
+
+  }
 
   return password;
 }
 
+console.log(password)
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -43,3 +54,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
+
+// Pseudocode
+
+// The user wants to generate a random password
+// The user will be asked how many characters they would like their password to be with a min of 8 and a max of 128.
+// The computer will store the users input
+// Next the user will be asked if they want numbers to be included in their password
+// The computer will store the user input
+// Next the user will be asked if they want UPPER case letters included in their password
+// The computer will store the user input
+// Next the user will be asked if they want LOWER case letters included in their password
+// The computer will store the user input
+// Next the user will be asked if they want to Special characters included in their password
+// The computer will store the user input
+// The computer will then take the users input and generate a random password that fits the users criteria
+// The password will display in the box on the site.
