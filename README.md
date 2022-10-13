a9da277018da0e63d32a9b16d90a02b2d0428a76
fredkamm <fred.kamm95@gmail.com>
2022-10-13 18:36:19 -0400

    updated

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Provide a short description explaining the what, why, and how of your project. U
 
 - The goal in the project was to create a functioning password generator that meets the users criteria.
 - I helped build this site because a random password generator that includes upper and lower case letters, special characters and numbers. giving the user a password to user for various accounts makes it almost impossible for someone to hack into their accounts.
-- The problem with the app orginally was that it was not generating a password at all. Currently it is not working to the acceptable standards but it is generating a password at the moment.
+- The problem with the app originally was that it was not generating a password at all. Currently it is not working to the acceptable standards but it is generating a password at the moment.
 - The one thing i learned is that javascript is not easy and it an important piece to making sure your site is running successfully.
 
 
@@ -15,9 +15,7 @@ Provide a short description explaining the what, why, and how of your project. U
 The way to use this site correctly would be to have the user enter in the desired about of character they would like in their password. Then they will be asked if they would like to include uppercase letters, lowercase letters, numbers, and/or special characters. the users input will then be logged so that the password generated will meet the users criteria. after the user answers all the prompts they will be asked to click the 'generate password' button which will then display the password that meets the users criteria.
 
 '
-    md![alt text](./assets/images/Screen%20Shot%202022-07-21%20at%207.57.50%20PM.png)
-    md![alt text](./assets/images/Screen%20Shot%202022-07-21%20at%207.58.04%20PM.png)
-    md![alt text](./assets/images/Screen%20Shot%202022-07-21%20at%207.58.15%20PM.png)
+    md![alt text](./assets/images//Screen%20Shot%202022-10-13%20at%206.33.21%20PM.png)
     
 So i'm able to have a password appear in the generator, and the users choices are registering correctly and generating random numbers. the only thing that is missing that i cant seem to figure out is the passwordLength and implementing it with the rest of the code for it to run as we want it.
 
--- assets/script.js
+++ assets/script.js
@@ -1,80 +1,60 @@
 // Assignment Code
-var generateBtn = document.querySelector('#generate');
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-// creating the function that generates the password
-function generatePassword(){
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-  var password= '';
-  var choices = [];
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-  //  variables for each character
-  var upperCase = ['A','B','C', 'D', 'E', 'F', 'G', 'H', 'I','J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k','l','m','n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
-  var specialChars = ['~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', '/', ';', ':', '"', '<', '>', ',', '.', '?' ];
-  var numberChars =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+})
 
-  // prompt for the password length
-  var passLength = prompt('enter desired password (min. 8 and a max of 128)');
-  if (passLength < 8 || passLength > 128){
-    alert("invalid password length, please try again");
-    return "";
-  }
-
-  // confirming the char types
-  if (confirm("do you want to include uppercase letters?")) {
-    choices = choices.concat(upperCase);
-    password += upperCase[Math.floor(Math.random()*upperCase.length)];
-  }
-  if (confirm("do you want to include lowercase letters?")) {
-    choices = choices.concat(lowerCase);
-    password += lowerCase[Math.floor(Math.random()*lowerCase.length)];
-  }
-  if (confirm("do you want to include numbers?")) {
-    choices = choices.concat(numberChars);
-    password += numberChars[Math.floor(Math.random()*numberChars.length)];
-  }
-  if (confirm("do you want to include special characters?")) {
-    choices = choices.concat(specialChars);
-    password += specialChars[Math.floor(Math.random()*specialChars.length)];
-  }
-
-  // notifying if they dont choosing anything they need to choose a character
-  if (password.length === 0){
-    alert('you must choose at least one character');
-    return '';
-  }
-//  how to add the input length to the choices 
-  while (password.length < passLength) {
-    password += choices[Math.floor(Math.random()*choices.length)];
-  }
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
-  return password;
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
-  passwordText.value = password;
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
-// Pseudocode
-
-// The user wants to generate a random password
-// The user will be asked how many characters they would like their password to be with a min of 8 and a max of 128.
-// The computer will store the users input
-// Next the user will be asked if they want numbers to be included in their password
-// The computer will store the user input
-// Next the user will be asked if they want UPPER case letters included in their password
-// The computer will store the user input
-// Next the user will be asked if they want LOWER case letters included in their password
-// The computer will store the user input
-// Next the user will be asked if they want to Special characters included in their password
-// The computer will store the user input
-// The computer will then take the users input and generate a random password that fits the users criteria
-// The password will display in the box on the site.
-
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
